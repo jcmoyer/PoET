c6966abb1ceaa9b7b14b6868bafcea7361ef35ce
J.C. Moyer <jmoyer1992@gmail.com>
2012-07-29 00:39:18 -0400

    Use single underscore for non-visible symbols

--- ggpk.py
+++ ggpk.py
@@ -35,7 +35,7 @@ class File:
 
   def __read_fs(self):
     self.fd.seek(self.header.rootoffs)
-    self.__root_dir = read_entry(self.fd)
+    self.__root_dir = _read_entry(self.fd)
 
   def root(self):
     return self.__root_dir
@@ -83,46 +83,46 @@ class FileEntry:
     self.name  = name
     self.where = where
 
-def decode_str(s):
+def _decode_str(s):
   return s.decode('utf16')
 
-def read_struct(fd, s):
+def _read_struct(fd, s):
   buf = fd.read(s.size)
   return s.unpack_from(buf)
 
 # Struct that represents PDIR data.
 # Name length U32 | Child count U32
-__pdir_info_struct = struct.Struct('<iI')
+_pdir_info_struct = struct.Struct('<iI')
 # Struct that points to PDIR children.
 # Checksum U32    | Child offset U64
-__pdir_child_struct = struct.Struct('<IQ')
-def read_pdir(fd):
-  namesize, childcount = read_struct(fd, __pdir_info_struct)
+_pdir_child_struct = struct.Struct('<IQ')
+def _read_pdir(fd):
+  namesize, childcount = _read_struct(fd, _pdir_info_struct)
   # 0x20 bytes that I don't know, but they seem unimportant
   fd.seek(0x20, 1)
   # Read name
   name = fd.read(namesize * 2)
-  name = decode_str(name).rstrip('\x00')
+  name = _decode_str(name).rstrip('\x00')
   # Read children
   children = []
   for n in range(childcount):
-    _, childoffs = read_struct(fd, __pdir_child_struct)
+    _, childoffs = _read_struct(fd, _pdir_child_struct)
     nextpos = fd.tell()
     # Read the child right now
     fd.seek(childoffs)
-    children.append(read_entry(fd))
+    children.append(_read_entry(fd))
     fd.seek(nextpos)
   return DirectoryEntry(name, children)
 
 # Struct that describes the filename length.
 # Name length U32
-__file_info_struct = struct.Struct('<i')
-def read_file(fd, nextoffs):
-  namesize = read_struct(fd, __file_info_struct)[0]
+_file_info_struct = struct.Struct('<i')
+def _read_file(fd, nextoffs):
+  namesize = _read_struct(fd, _file_info_struct)[0]
   # 0x20 unknown bytes
   fd.seek(0x20, 1)
   name = fd.read(namesize * 2)
-  name = decode_str(name).rstrip('\x00')
+  name = _decode_str(name).rstrip('\x00')
   # Everything from here to the next offset is file data.
   offs = fd.tell()
   size = nextoffs - offs
@@ -130,14 +130,14 @@ def read_file(fd, nextoffs):
 
 # Entry header struct
 # Next offset U32 | Type String 32
-__entry_struct = struct.Struct('<I4s')
-def read_entry(fd):
+_entry_struct = struct.Struct('<I4s')
+def _read_entry(fd):
   curoffs = fd.tell()
-  nextoffs, ent_type = read_struct(fd, __entry_struct)
+  nextoffs, ent_type = _read_struct(fd, _entry_struct)
   if ent_type == 'PDIR':
-    return read_pdir(fd)
+    return _read_pdir(fd)
   elif ent_type == 'FILE':
-    return read_file(fd, curoffs + nextoffs)
+    return _read_file(fd, curoffs + nextoffs)
   elif ent_type == 'FREE':
     return None
 
